93273c172203f4a4ce7bd9141fbcfeed0fd1e2d5
thomasdelatte <thomasdelatte@hotmail.com>
2020-05-25 16:50:23 +0200

    deleted request file

--- src/request.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-req = requests.post('https://eu-text-generator-lpl4nmyypq-ew.a.run.app',
-                    json={'length': 300, 'temperature': 0.8, 'prefix': '<Title:> COMMISSION IMPLEMENTING DECISION'})
-text = req.json()['text']
-print(text)
